7335542e43b816dea92af0fbb85424a8944a5ac2
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-12 12:13:43 -0300

    Update and rename eth_gasPrice.js to puffs_gasPrice.js

--- test/puffs_gasPrice.js
+++ test/puffs_gasPrice.js
@@ -3,7 +3,7 @@ var config = require('../lib/config'),
     assert = require('chai').assert;
 
 // METHOD
-var method = 'eth_gasPrice';
+var method = 'puffs_gasPrice';
 
 // TEST
 var asyncTest = function(host, done){
