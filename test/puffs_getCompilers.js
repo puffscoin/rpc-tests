cd3c912f539c8d14c98dbc8ecf2343abdff0bcc9
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-12 12:16:10 -0300

    Update and rename eth_getCompilers.js to puffs_getCompilers.js

--- test/puffs_getCompilers.js
+++ test/puffs_getCompilers.js
@@ -3,7 +3,7 @@ var config = require('../lib/config'),
     assert = require('chai').assert;
 
 // METHOD
-var method = 'eth_getCompilers';
+var method = 'puffs_getCompilers';
 
 // TEST
 var asyncTest = function(host, done){
