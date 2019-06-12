054f1e2d64b254836700c81901278a041af78959
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-11 22:45:37 -0300

    Update RPC_Batch_call.js

--- test/RPC_Batch_call.js
+++ test/RPC_Batch_call.js
@@ -5,14 +5,14 @@ var config = require('../lib/config'),
 // TEST
 var asyncTest = function(host, done){
     Helpers.send(host, [{
-        id: config.rpcMessageId++, jsonrpc: "2.0", method: 'eth_accounts',
+        id: config.rpcMessageId++, jsonrpc: "2.0", method: 'puffs_accounts',
         
         // PARAMETERS
         params: []
 
     },
     {
-        id: config.rpcMessageId++, jsonrpc: "2.0", method: 'eth_getBalance',
+        id: config.rpcMessageId++, jsonrpc: "2.0", method: 'puffs_getBalance',
         
         // PARAMETERS
         params: ['0xbcde5374fce5edbc8e2a8697c15331677e6ebf0b','latest']
@@ -26,7 +26,7 @@ var asyncTest = function(host, done){
 
     },
     {
-        id: config.rpcMessageId++, jsonrpc: "2.0", method: 'eth_gasPrice',
+        id: config.rpcMessageId++, jsonrpc: "2.0", method: 'puffs_gasPrice',
         
         // PARAMETERS
         params: []
@@ -36,10 +36,10 @@ var asyncTest = function(host, done){
         assert.isArray(result, 'is array');
         assert.property(result[0], 'result', (result.error) ? result.error.message : 'error');
 
-        // eth_accounts
+        // puffs_accounts
         assert.isArray(result[0].result, 'is array');
 
-        // eth_getBalance
+        // puffs_getBalance
         var balance = config.testBlocks.postState['bcde5374fce5edbc8e2a8697c15331677e6ebf0b'].balance;
         assert.isNumber(+result[1].result, 'is a number');
         assert.equal(+result[1].result, balance, 'is the same as '+ balance);
@@ -47,7 +47,7 @@ var asyncTest = function(host, done){
         // net_listening
         assert.isBoolean(result[2].result, 'is boolean');
 
-        // eth_gasPrice
+        // puffs_gasPrice
         assert.isNumber(+result[3].result, 'is a number');
         assert.isAbove(+result[3].result, 0, 'is a number');
 
