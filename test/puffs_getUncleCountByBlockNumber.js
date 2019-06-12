515e0a8ecdf69cac434507d87072274789f06faa
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-12 12:27:33 -0300

    Update and rename eth_getUncleCountByBlockNumber.js to puffs_getUncleCountByBlockNumber.js

--- test/puffs_getUncleCountByBlockNumber.js
+++ test/puffs_getUncleCountByBlockNumber.js
@@ -4,7 +4,7 @@ var config = require('../lib/config'),
     _ = require('underscore');
 
 // METHOD
-var method = 'eth_getUncleCountByBlockNumber';
+var method = 'puffs_getUncleCountByBlockNumber';
 
 
 // TEST
