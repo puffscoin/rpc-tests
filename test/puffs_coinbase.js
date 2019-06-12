fdeac4d8e1ca115593e9e9b5fda3f8921cb21806
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-12 12:13:27 -0300

    Update and rename eth_coinbase.js to puffs_coinbase.js

--- test/puffs_coinbase.js
+++ test/puffs_coinbase.js
@@ -3,7 +3,7 @@ var config = require('../lib/config'),
     assert = require('chai').assert;
 
 // METHOD
-var method = 'eth_coinbase';
+var method = 'puffs_coinbase';
 
 // TEST
 var asyncTest = function(host, done){
